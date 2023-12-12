4b729d81abcab9754819156bfb8c2d7bdae61faf
James Alonzo <james.alonzo@marquette.edu>
2023-12-12 14:59:18 -0600

    added comments to cart

--- ClassProject/Model/Cart.cs
+++ ClassProject/Model/Cart.cs
@@ -9,15 +9,15 @@ namespace ClassProject.Model;
 public class Cart
 {
     
-    public List<Item> cartItems ;
+    public List<Item> cartItems ;//declares a reference variable for a list
 
     public Cart()
     {
-        cartItems = new List<Item>();
+        cartItems = new List<Item>();//assigns reference varibale to a new list
         
     }
 
-    public void AddItemToCart(string itemName, Inventory inventory)
+    public void AddItemToCart(string itemName, Inventory inventory)// method that adds items to the cart using their name and the list they are going to
     {
         List<Item> itemsInStock = inventory.GetItemsInStock();
         Item foundItem = itemsInStock.Find(item => item.ItemName.Equals(itemName, StringComparison.OrdinalIgnoreCase));
@@ -25,7 +25,7 @@ public class Cart
         if (foundItem != null)
         {
             cartItems.Add(foundItem);
-            Console.WriteLine($"{itemName} added to cart.");
+            Console.WriteLine($"{itemName} added to cart.");//if else statement to check if the item is in stock or not
         }
         else
         {
@@ -33,7 +33,7 @@ public class Cart
         }
     }
 
-    public void DisplayCartItems()
+    public void DisplayCartItems()//prints each item in the cart one by one with price
     {
         Console.WriteLine("Items in Cart:");
         foreach (var item in cartItems)
